d2297843701937b66ef1c8203b4d393e7f4eb6db
Yaresko Vlad <yareskofamily@gmail.com>
2023-02-12 23:08:50 +0200

    Правки футера , и JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,41 @@
-(function ($) {
+$('.menu__btn').on('click', function() {
+	$(this).toggleClass('active').
+	next().
+	toggleClass('active');
+});
 
+let $slideItems = $('.carousel-slide');
+let $nextBtn = $('.next.carousel-control');
+let $prevBtn = $('.prev.carousel-control');
+let currentSlide = 0;
 
+let gotoSlide = (n) => {
+	$slideItems[currentSlide].classList.toggle('active');
+	currentSlide = (n + $slideItems.lenght) % $slideItems.lenght;
+	$slideItems[currentSlide].classList.toggle('active');
+}
 
-})(jQuery);
+let nextSlide = () => {
+	gotoSlide(currentSlide + 1);
+}
+
+let prevSlide = () => {
+	gotoSlide(currentSlide - 1);
+}
+
+let slideInterval = setInterval(nextSlide, 5000);
+
+let nextClickHandler = () => {
+	nextSlide();
+	clearInterval(slideInterval);
+	slideInterval = setInterval(nextSlide, 5000);
+}
+
+let prevClickHandler = () => {
+	prevSlide();
+	clearInterval(slideInterval);
+	slideInterval = setInterval(nextSlide, 5000)
+}
+
+$nextBtn.on('click', nextClickHandler);
+$prevBtn.on('click', prevClickHandler);
